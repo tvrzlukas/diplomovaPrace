d870369efeca0b5b2471d26f3581a13992dbaff8
tvrzlukas <tvrzlukas@icloud.com>
2020-10-19 16:26:01 +0200

    prvni_verze

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function checked(){
+
+    if ((document.getElementById('rememberMe').checked = true ) = 1 ){
+        alert('nastaveno ANO');
+    } else {
+        alert('nastaveno NE');
+
+    }
+    
+}
+
